24b5590492fbe084c55d15ce2fe5986db21a6c95
chayangge <chayangge@163.com>
2017-03-28 21:02:52 +0800

    默认提交

--- app.js
+++ app.js
@@ -4,93 +4,197 @@ var cheerio = require("cheerio");
 var superagent = require("superagent");
 var http = require("http");
 var sleep = require('system-sleep');
-// var sleep = require("sleep");
-var async = require("async");
 
 var wechat_id = process.argv[2];
 console.log(wechat_id)
-var url = "http://chuansong.me/account/" + wechat_id;
-var articleList = {};
-var pageList = {};
 
-function getPage(url) {
-    // sleep.sleep(1)
-    sleep(1000)
-    superagent.get(url)
-        .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
-        .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
-        .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
-        .set('Connection', 'keep-alive')
-        .set('Accept-Encoding', 'gzip, deflate, sdch')
-        .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-        .end(function(err, result) {
-            fs.mkdir(wechat_id, function(err, files) {
-                var $ = cheerio.load(result.text, {
-                    decodeEntities: false
+var crawler = (function(wechat_id) {
+    var url = "http://chuansong.me/account/" + wechat_id;
+    var articleList = {};
+    var pageList = {};
+
+    return {
+        init: function() {
+            this.getPage(url);
+        },
+        getPage: function(url) {
+            sleep(1000);
+            var _this = this;
+            superagent.get(url)
+                .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
+                .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
+                .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
+                .set('Connection', 'keep-alive')
+                .set('Accept-Encoding', 'gzip, deflate, sdch')
+                .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+                .end(function(err, result) {
+                    fs.mkdir(wechat_id, function(err, files) {
+                        var $ = cheerio.load(result.text, {
+                            decodeEntities: false
+                        });
+                        var pages = $(".w4_5 > span > a"); //.html();
+                        var articles = $(".question_link");
+
+                        articles.each(function(id, article) {
+                            var url = $(article).attr("href");
+                            if (articleList[url] == null) {
+                                articleList[url] = "http://chuansong.me" + url;
+                                sleep(1000);
+                                _this.getArticle("http://chuansong.me" + url)
+                            }
+                        });
+
+                        pages.each(function(id, element) {
+                            var url = $(element).attr("href");
+                            if (pageList[url] == null) {
+                                pageList[url] = "http://chuansong.me" + url;
+                                sleep(1000);
+                                _this.getPage("http://chuansong.me" + url);
+                            }
+                        });
+                    });
                 });
-                var pages = $(".w4_5 > span > a"); //.html();
-                var articles = $(".question_link");
-
-                articles.each(function(id, article) {
-                    var url = $(article).attr("href");
-                    if (articleList[url] == null) {
-                        articleList[url] = "http://chuansong.me" + url;
-                        sleep(1000)
-                        getArticle("http://chuansong.me" + url)
+        },
+        getArticle: function(url) {
+            sleep(1000);
+            superagent.get(url)
+                .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
+                .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
+                .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
+                .set('Connection', 'keep-alive')
+                .set('Accept-Encoding', 'gzip, deflate, sdch')
+                .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+                .end(function(err, result) {
+                    if (err != null) {
+                        console.log(err);
+                        return;
                     }
+                    var $ = cheerio.load(result.text, {
+                        decodeEntities: false
+                    });
+                    var name = $("#activity-name").html().trim();
+                    name = name.replace(/\//, " ");
+                    console.log(name)
+                    var textEle = $(".rich_media_content > p");
+                    fs.appendFile("./" + wechat_id + '/' + name, name + "\n \n");
+
+                    textEle.each(function(id, article) {
+                        var text = $(article).text();
+                        fs.appendFile("./" + wechat_id + '/' + name, text + "\n");
+                    });
+                    sleep(300)
                 });
-
-                pages.each(function(id, element) {
-                    var url = $(element).attr("href");
-                    if (pageList[url] == null) {
-                        pageList[url] = "http://chuansong.me" + url;
-
-                        sleep(1100)
-                        getPage("http://chuansong.me" + url);
-                    }
-                });
-            });
-        });
-}
-
-
-function getArticle(url) {
-    // sleep.sleep(1)
-    sleep(1000)
-    superagent.get(url)
-        .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
-        .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
-        .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
-        .set('Connection', 'keep-alive')
-        .set('Accept-Encoding', 'gzip, deflate, sdch')
-        .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-        .end(function(err, result) {
-            if (err != null) {
-                console.log(err);
-                return;
-            }
-
-            var $ = cheerio.load(result.text, {
-                decodeEntities: false
-            });
-            var name = $("#activity-name").html().trim();
-            name = name.replace(/\//, " ");
-            console.log(name)
-            var textEle = $(".rich_media_content > p");
-            fs.appendFile("./" + wechat_id + '/' + name, name + "\n \n");
-
-            textEle.each(function(id, article) {
-                var text = $(article).text();
-                fs.appendFile("./" + wechat_id + '/' + name, text + "\n");
-            });
             sleep(300)
+        }
 
-        });
-    sleep(300)
-}
+    }
+})(wechat_id);
+
+crawler.init();
 
 
 
-getPage(url)
+// var url = "http://chuansong.me/account/" + wechat_id;
+// var articleList = {};
+// var pageList = {};
+
+// function getPage(url) {
+//     sleep(1000)
+//     superagent.get(url)
+//         .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
+//         .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
+//         .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
+//         .set('Connection', 'keep-alive')
+//         .set('Accept-Encoding', 'gzip, deflate, sdch')
+//         .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+//         .end(function(err, result) {
+//             fs.mkdir(wechat_id, function(err, files) {
+//                 var $ = cheerio.load(result.text, {
+//                     decodeEntities: false
+//                 });
+//                 var pages = $(".w4_5 > span > a"); //.html();
+//                 var articles = $(".question_link");
+
+//                 articles.each(function(id, article) {
+//                     var url = $(article).attr("href");
+//                     if (articleList[url] == null) {
+//                         articleList[url] = "http://chuansong.me" + url;
+//                         sleep(1000)
+//                         getArticle("http://chuansong.me" + url)
+//                     }
+//                 });
+
+//                 pages.each(function(id, element) {
+//                     var url = $(element).attr("href");
+//                     if (pageList[url] == null) {
+//                         pageList[url] = "http://chuansong.me" + url;
+
+//                         sleep(1100)
+//                         getPage("http://chuansong.me" + url);
+//                     }
+//                 });
+//             });
+//         });
+// }
+
+
+// function getArticle(url) {
+//     sleep(1000)
+//     superagent.get(url)
+//         .set('Cookie', 'pac_uid=1_296961962; tvfe_boss_uuid=fa639707d46a593a; gaduid=57e233c12e361; ts_uid=7972323162; 3g_guest_id=-8959557366479556608; sd_userid=2221479884684448; sd_cookie_crttime=1479884684448; mobileUV=1_159532ed952_608ef; noticeLoginFlag=1; remember_acct=chuanjinw%40sina.com; pgv_pvi=6468546560; RK=sTuKTBHLcq; ptcz=914f54747dd08a9cd8494895a874333736c85055c8ce787a519d62d0a4d9b216; pt2gguin=o0296961962; o_cookie=296961962; pgv_info=ssid=s291503616; pgv_pvid=3796909651')
+//         .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')
+//         .set('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2')
+//         .set('Connection', 'keep-alive')
+//         .set('Accept-Encoding', 'gzip, deflate, sdch')
+//         .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+//         .end(function(err, result) {
+//             if (err != null) {
+//                 console.log(err);
+//                 return;
+//             }
+//             var $ = cheerio.load(result.text, {
+//                 decodeEntities: false
+//             });
+//             var name = $("#activity-name").html().trim();
+//             name = name.replace(/\//, " ");
+//             console.log(name)
+//             var textEle = $(".rich_media_content > p");
+//             fs.appendFile("./" + wechat_id + '/' + name, name + "\n \n");
+
+//             textEle.each(function(id, article) {
+//                 var text = $(article).text();
+//                 fs.appendFile("./" + wechat_id + '/' + name, text + "\n");
+//             });
+//             sleep(300)
+
+//         });
+//     sleep(300)
+// }
+
+
+
+// var a = (function() {
+//     var aa = 1
+//     var bb = 2;
+//     return {
+//         init: function() {
+//             console.log(aa)
+//             console.log(this)
+//             this.add()
+//         },
+//         add: function() {
+//             console.log("add")
+//         }
+//     }
+// })()
+
+// a.init();
+
+
+
+
+
+
+// getPage(url)
 
 // http.createServer(start).listen(9000);
